84ec7dae0b30bb1d1caa5ba2040eee36f559252a
Александр Деревянкин <60281708+animalsgame@users.noreply.github.com>
2025-01-06 13:22:16 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,302 @@
+﻿// функция нужна чтобы легко можно было выбирать случайный элемент из массива
+Array.prototype.random=function(){
+return this[Math.floor(Math.random() * this.length)];
+};
+
+// функция возвращает случайное число в диапазоне от min до max, например 10,20 вернёт число от 10 до 20 (включительно)
+function randomMinMax(min, max){
+return Math.round(min + (max - min) * Math.random());
+}
+
+// текущее время (timestamp) в секундах (начиная с 1 января 1970 года)
+function time(){
+return Math.floor(Date.now() / 1000);
+}
+
+const fs=require('fs'); // для работы с файловой системой
+const {Bot, log} = require('./bot'); // сам бот
+var bot=new Bot();
+var adminCmds={};
+
+
+// является ли отправитель другом, без добавления в друзья бот не может дарить подарки, отправлять сообщения в любое время
+function checkDrug(user){
+if(bot.botInfo){
+if(user.id==bot.botInfo.owner || user.botDrug)return true;
+}
+return false;
+}
+
+// проверка прав админа (владелец бота)
+function checkAdmin(user){
+var adminIds=[]; // если нужно выдать права админа, укажите список id игроков через запятую, но будьте осторожны, получив управление - игроки смогут менять ник, инфу о себе, и даже кикнуть бота
+
+// проверяем id отправителя с id владельца бота (свойство owner в объекте botInfo)
+if(bot.botInfo && user.id==bot.botInfo.owner)return true;
+
+// если не нашли, проверяем id из списка
+if(adminIds.indexOf(user.id)>-1)return true;
+
+// если не нашли, значит точно отправитель без прав админа, возвращаем false
+return false;
+}
+
+function cmdWord(name,value,props){
+if(!props)props={};
+if(name && value){
+bot.cmd(name,(user,words)=>{
+if(words.length==0)return {...props,msg:value,type:'message'};
+});
+}
+}
+
+function addAdminCmd(name, method){
+adminCmds[name]={type:'api', v:method};
+}
+
+addAdminCmd('ник', 'bot.nickChange');
+addAdminCmd('цвет', 'bot.nickColorChange');
+addAdminCmd('пол', 'bot.polChange');
+addAdminCmd('осебе', 'bot.textInfoChange');
+addAdminCmd('друзья','bot.friends');
+
+
+// добавляем команды которые обрабатывают одно слово, например если ввести пинг, бот отправит ПОНГ, третий параметр это свойства, туда можно передать цвет сообщения
+
+var mainProps={color:['#FFFFFF']}; // FFFFFF в hex формате это белый цвет (255 255 255) 
+
+cmdWord('пинг', 'ПОНГ', mainProps);
+cmdWord('кинг', 'КОНГ', mainProps);
+cmdWord('кошка', 'МЫШКА', {color:['#f1a0b3', '#FFFF00']}); // пусть текст МЫШКА будет цветным
+
+// запуск бота
+bot.run((info)=>{
+// когда авторизация прошла успешно, в info будет передан объект бота, и можно уже начинать работать!
+log('бот', info);
+
+// отправляем сообщение владельцу что бот в сети. Владельца мы знаем, в объекте бота есть свойство owner, это и есть id владельца, но можно ввести другой id игрока, если бот в друзьях то сообщение дойдёт!
+bot.sendMessage(info.owner, 'Мой владелец, я в сети*em3*', {color:['#f1a0b3', '#FFFF00', '#54EFF7']});
+});
+
+// Команды
+
+bot.cmd('бот',(user,words)=>{
+
+if(words.length==0){ // если только команда бот (и нет больше слов в массиве words)
+return {msg:'На месте*em1*', color:['#f1a0b3']};
+}
+
+if(checkAdmin(user)){ // если отправитель владелец бота (админ) то разрешаем управление ботом
+if(words && words.length>0){
+var cmd=words.shift();
+var val=words.join(' ');
+if(cmd=='кик' && words.length==0){ // кикнуть бота, после выполнения соединение с ботом закрывается
+var tm=-1;
+var cbExit=()=>{
+if(tm!=-1)clearTimeout(tm);
+process.exit();
+};
+tm=setTimeout(cbExit,3000);
+bot.sendMessage(user.id,'Пока!',{color:'#FFFFFF'},cbExit);
+return true;
+}
+
+if(cmd && cmd in adminCmds){
+var cmdInfo=adminCmds[cmd];
+var methodApi=(cmdInfo.type=='api') ? cmdInfo.v : null;
+if(methodApi){
+// если найдена админ команда, обращаемся к api
+bot.api(methodApi,{v:val},(res)=>{
+//console.log('api result',res);
+var msgTxt=null;
+if(res && res.error && res.error.msg){ // если произошла ошибка и есть текст ошибки, то выбираем его
+msgTxt=res.error.msg;
+}
+else if(res && res.msg){ // если не ошибка, и есть какой-то текст, выбираем его
+msgTxt=res.msg;
+}
+if(cmd=='друзья'){ // если команда друзья (это список id игроков кто добавил бота в друзья) для удобства массив переводим в строку (текст) через .join
+if(res && Array.isArray(res)){
+if(res.length>0)msgTxt='Всего '+res.length+' - '+res.join(',');
+else msgTxt='Нет друзей';
+}
+}
+if(msgTxt){ // если есть текст (может это ошибка от api, а может информация) отправляем этот текст 
+bot.sendMessage(user.id, msgTxt, {color:'#FFFFFF'});
+}
+});
+}
+return true;
+}
+}
+}
+});
+
+bot.cmd('данет',(user,words)=>{
+if(words.length>0){
+var rnd=Math.random(); // выдаёт случайное число от 0 до 0.99
+var txt=words.join(' ');
+var v='да'; // по умолчанию "да"
+if(rnd>0.5)v='нет'; // если число больше 0.5 тогда будет "нет"
+if(rnd>0.9)v='не знаю'; // если число больше 0.9 (оно выпадает не часто) тогда будет "не знаю"
+return '"'+txt+'" ('+v+')';
+}
+});
+
+bot.cmd('выбери',(user,words)=>{
+var txt=words.join(' '); // переводим все слова из массива в строку
+var spl=txt.split(' или '); // разделяем через " или " например апельсин или яблоко, пробелы обязательно, иначе команда будет работать подругому, вместо "или" может быть любое слово, например гав, тогда логика команды поменяется, нужно будет писать выбери апельсин гав яблоко
+if(spl.length>1){ // если в массиве после разделения больше одного слова, тогда можно отправлять результат
+return 'Я выбираю: '+spl.random();
+}
+});
+
+bot.cmd('скажи',(user,words)=>{
+var txt=words.join(' '); // переводим все слова из массива в строку
+if(txt){
+var colorsArr=[ ['#f1a0b3', '#FFFF00'], ['#FFFFFF', '#54EFF7'] ]; // массив, в котором хранятся цвета тоже в виде массива
+var color=colorsArr.random(); // выбираем случайный массив
+// и отправляем этот же текст, только уже делаем его цветным
+return {color:color, msg:txt};
+}
+});
+
+bot.cmd('рандом',(user,words)=>{
+var n1=0;
+var n2=0;
+if(words.length>0){
+if(words.length>1){ // если передано больше 1 числа (от-до)
+n1=parseInt(words[0]) || 0;
+n2=parseInt(words[1]) || 0;
+}else{ // если только одно число
+n2=parseInt(words[0]) || 0;
+}
+if(n1!=n2 && n2>n1){ // если число n1 не равно n2 И ЕСЛИ (&&) второе число больше первого, тогда генерируем случайное число от n1 до n2
+return randomMinMax(n1,n2);
+}
+}
+});
+
+// маленький калькулятор, просто складывает два числа, например "плюс 1 3" = 4
+bot.cmd('плюс',(user,words)=>{
+var a=parseInt(words[0]) || 0;
+var b=parseInt(words[1]) || 0;
+var result=a+b;
+return 'ответ: '+result;
+});
+
+bot.cmd('магия',(user,words)=>{
+if(words.length==0){
+var polStr='женский';
+if(user.pol=='м')polStr='мужской';
+else if(user.pol=='н')polStr='нло';
+
+var rolesObj={ml_mod:'младший мч', ml_modmaps:'младший мк', mod:'старший мч', modmaps:'старший мк', admin:'админ'};
+
+var rolesArr=[]; // список ролей (должности)
+if(user.roles){
+for (var i = 0; i < user.roles.length; i++) {
+var role=user.roles[i];
+if(role && role in rolesObj){
+var roleName=rolesObj[role]; // если должность найдена, добавляем в список
+rolesArr.push(roleName);
+}
+}
+}
+
+var arr=[
+'твой id: '+user.id,
+'ник: '+user.nick,
+'пол: '+polStr,
+'опыт: '+user.opyt,
+'уровень: '+user.level,
+'уровень популярности: '+user.popularLevel
+];
+
+if(rolesArr.length>0){ // если есть хоть одна должность
+arr.push('есть должности: '+rolesArr.join(', '));
+}
+
+var msg='Вижу вижу... '+arr.join(', ')+'.';
+
+if(user.botDrug){ // если отправитель в друзьях, добавляем ещё такой текст
+msg+=' А ещё ты мой друг!';
+}
+
+return {msg:msg, color:['#ffdf52', '#FD92FE', '#54EFF7', '#68ff5d']};
+}
+});
+
+var giftsQueryUsers={}; // здесь храним запросы (от каждого игрока) на получение подарков, нам достаточно только хранить время, чтобы не смогли взять много раз подарок
+
+bot.cmd('хочу подарок',(user,words)=>{
+if(checkDrug(user)){
+var userid=user.id;
+var ts=time(); // текущее время (в секундах)
+var timeMinutes=[3,4,5]; // сколько минут будет ждать игрок до следующего подарка
+var waitTime=60 * timeMinutes.random(); // в минуте 60 секунд, поэтому 60 умножаем на количество минут
+var endTime=giftsQueryUsers[userid] || 0; // проверяем, было обращение от такого игрока (или нет) если да значит получим время когда можно брать следующий подарок, иначе 0 (если 0 тогда тоже разрешит брать подарок, так как текущее время будет больше 0)
+if(ts>endTime){ // можно брать подарок
+giftsQueryUsers[userid]=ts+waitTime; // берём текущее время и прибавляем время ожидания (сколько будет ждать игрок до следующего подарка)
+
+var ids=[1,4]; // список id подарков, id можно получить в игре (в окне выбора подарка) id нужен чтобы бот мог отправить подарок!
+
+var itemid=ids.random(); // берём случайный id подарка
+
+// отправляем подарок!
+bot.sendGift(userid,itemid,(res)=>{
+if(res.error){
+if(res.error.msg){ // если произошла ошибка и есть текст ошибки (например недостаточно валюты) то отправляем этот текст
+bot.sendMessage(userid,res.error.msg,{});
+}
+}else if(res.ok){ // если всё хорошо
+bot.sendMessage(userid,'Держи!',{color:'#FFFFFF'});
+}
+});
+}else{ // если ещё не пришло время получения подарка
+var seconds=endTime-ts; // берём время окончания и текущее время, и получаем уже секунды
+var minuts=Math.floor(seconds/60); // получаем минуты, секунды делим на 60 (в минуте 60 секунд)
+seconds-=minuts*60; // отнимаем кол-во минут, чтобы было корректное значение для секунд
+
+var arr=[];
+if(minuts>0)arr.push(minuts+' мин.');
+if(seconds>0)arr.push(seconds+' сек.');
+
+var msgsArr=['Я тоже хочу!', 'Хотеть не вредно.'];
+var msg=msgsArr.random(); // берём случайный текст из массива
+msg+=' Нужно ещё подождать ';
+msg+=arr2.join(' ');
+return {msg:msg, color:['#FFFFFF']};
+}
+}
+
+// если бот не в друзьях у получателя
+return {msg:'Я не могу дать подарок, добавь меня в друзья!', color:['#f1a0b3', '#FFFF00']};
+});
+
+// Обработка событий
+
+// бот может принимать файлы, обработать получение можно в событии file, в этом примере полученный файл сохраняем в папке где и запущен бот, только немного меняем название файла (вместе с файлом приходит объект отправителя где можно узнать id игрока) этот id и берём, так мы будем знать от кого пришёл файл
+
+bot.event('file',(o,data)=>{
+var userid=o.user.id;
+var nm='user_'+userid+'_'+o.name;
+var rr=/[\/\?<>\\:\*\|"]/g; // запрещённые символы убираем из имени файла (в windows, и не только) иначе файл не сохранится
+nm=nm.replace(rr,'');
+try{
+fs.writeFileSync(nm, data); // сохраняем файл
+}catch(e){
+// если произошла ошибка
+}
+});
+
+// если нужно сделать историю сообщений (все сообщения которые пишут боту можно получить в событии message) уберите /* */ ниже, и как только появится сообщение, оно будет добавлено в файл messages.txt (файл будет в папке откуда вы запустили бота)
+
+/*
+bot.event('message',(user,text)=>{
+try{
+fs.appendFileSync('messages.txt', 'id '+user.id+' ('+user.nick+') -> '+text+'\r\n');
+}catch(e){
+}
+});
+*/
---
+++ bot.js
@@ -0,0 +1,355 @@
+const {Socket} = require('net');
+const NetCmd={FRIEND:1,MSG:2,MSG_BINARY:3};
+const numBytes=4+4+1+1+4;
+
+function getCurrentTime(){
+var date=new Date();
+var strhour=date.getHours();
+var strmin=date.getMinutes();
+var strsec=date.getSeconds();
+if(date.getHours()<10)strhour='0'+date.getHours();
+if(date.getMinutes()<10)strmin='0'+date.getMinutes();
+if(date.getSeconds()<10)strsec='0'+date.getSeconds();
+return [strhour,strmin,strsec].join(':');
+}
+
+function log(){
+console.log(...[getCurrentTime(), ...arguments]);
+}
+
+function emptyCB(error){}
+
+function cryptBuffer(client,buf){
+if(buf && client.signBuffer){
+var len=client.signBuffer.length;
+for (var i = 0; i < buf.length; i++)buf[i]^=client.signBuffer[i%len];
+}
+}
+
+function genCallbackID(client){
+var v=0;
+do{
+v=10+Math.floor(Math.random()*1000000);
+}while(client.callbacks[v]);
+return v;
+}
+
+function sendPacket(client,packetid,type,data,cb){
+if(!cb)cb=emptyCB;
+if(client.client){
+var len=(data) ? data.length : 0;
+var buf=Buffer.alloc(numBytes+len);
+buf.writeUInt32LE(len);
+buf.writeInt32LE(packetid,4);
+buf.writeUInt8(type,8);
+if(data){
+cryptBuffer(client,data);
+data.copy(buf,numBytes);
+}
+client.client.write(buf,cb);
+}
+}
+
+function sendData(client,packetid,o,cb){
+if(o){
+var str=JSON.stringify(o);
+if(str){
+var buf=Buffer.from(str);
+var isGenID=packetid==0;
+var packid=isGenID ? genCallbackID(client) : packetid;
+
+if(isGenID){
+var obj={o:o,ts:Date.now()};
+if(typeof cb=='function')obj.cb=cb;
+client.callbacks[packid]=obj;
+}
+
+sendPacket(client,packid,NetCmd.MSG,buf);
+if(client.isLog)log('[packet '+packid+']','[send] ->',str);
+}
+}
+}
+
+function sendBinaryData(client,o,data,cb){
+if(!o)o={};
+if(data && data instanceof Buffer){
+var str=JSON.stringify(o);
+var bufStr=Buffer.from(str);
+
+var buf1=Buffer.alloc(4);
+buf1.writeInt32LE(bufStr.length);
+
+var buf=Buffer.concat([buf1,bufStr,data]);
+
+var packid=genCallbackID(client);
+var obj={o:o,ts:Date.now()};
+if(typeof cb=='function')obj.cb=cb;
+client.callbacks[packid]=obj;
+
+sendPacket(client,packid,NetCmd.MSG_BINARY,buf);
+if(client.isLog)log('[packet binary '+packid+']','[send] ->',str,data,'['+data.length+' bytes]');
+}
+}
+
+
+function readBuffer(socket, data, cb){
+var checkFlag=false;
+var chunkInfo=socket.chunkInfo;
+chunkInfo.buffer=Buffer.concat([chunkInfo.buffer, data]);
+
+do{
+checkFlag=false;
+var cmdType=chunkInfo.cmdType;
+if(chunkInfo.messageSize==0 && chunkInfo.buffer.length>=numBytes){
+chunkInfo.messageSize=chunkInfo.buffer.readInt32LE(0);
+chunkInfo.packetID=chunkInfo.buffer.readInt32LE(4);
+chunkInfo.cmdType=cmdType=chunkInfo.buffer.readUInt8(8);
+}
+
+if(chunkInfo.messageSize>0 && chunkInfo.buffer.length>=chunkInfo.messageSize+numBytes){
+var buffer=chunkInfo.buffer.subarray(numBytes,chunkInfo.messageSize+numBytes);
+if(cmdType==NetCmd.FRIEND){
+if(buffer && buffer.length>0)socket.signBuffer=buffer;
+log('сервер ->','давай!');
+log('клиент ->','передаю токен.');
+
+socket.isLog=false;
+sendData(socket,0,{cmd:'auth',token:socket.token});
+socket.isLog=true;
+}else{
+cryptBuffer(socket,buffer);
+}
+
+var packid=chunkInfo.packetID;
+
+chunkInfo.messageSize=0;
+chunkInfo.cmdType=0;
+chunkInfo.packetID=0;
+chunkInfo.buffer=chunkInfo.buffer.slice(buffer.length+numBytes);
+
+checkFlag=chunkInfo.buffer.length>0;
+
+if(cmdType==NetCmd.MSG){
+var obj=null;
+try{
+obj=JSON.parse(buffer.toString());
+}catch(e){
+}
+
+if(obj){
+var nm='data';
+var isEvent=false;
+var resObj=obj;
+if(obj.event && obj.data){
+isEvent=true;
+nm=obj.event;
+}
+if(obj.event=='data' && resObj.data)resObj=resObj.data;
+
+if(obj.event=='error'){
+var err=(obj.data) ? JSON.stringify(obj.data) : null;
+var eventType=(obj.data) ? obj.data.type : null;
+if(typeof eventType=='string' && eventType in socket.eventsObj)socket.eventsObj[eventType](obj.data);
+log('[packet '+packid+']','[error] ->',err);
+resObj={error:obj.data};
+}else if(obj.cmd=='auth' && obj.data){
+resObj=null;
+socket.botInfo=obj.data.bot;
+if(cb)cb(socket.botInfo);
+//resObj=obj.data;
+}else{
+if(isEvent)obj=obj.data;
+if(socket.isLog)log('[packet '+packid+']','['+nm+'] ->',JSON.stringify(obj));
+}
+
+if(packid in socket.callbacks){
+var cbObj=socket.callbacks[packid];
+delete socket.callbacks[packid];
+if(resObj && cbObj.cb)cbObj.cb(resObj);
+}
+
+if(isEvent){
+var isEmpty=true;
+var cbObj2=null;
+var msg=obj.msg;
+var msgOrig=msg;
+var userObj=obj.user;
+var spl=null;
+if(msg && userObj){
+spl=msg.split(' ');
+var cmdName=spl.shift();
+
+if('message' in socket.eventsObj)socket.eventsObj.message(userObj,msg);
+
+if(msg in socket.cmdsObj)cbObj2=socket.cmdsObj[msg];
+
+if(!cbObj2){
+var msgLower=msg.toLowerCase();
+if(msgLower in socket.cmdsObj)cbObj2=socket.cmdsObj[msgLower];
+}
+
+if(!cbObj2){
+if(cmdName){
+cmdName=cmdName.toLowerCase();
+msg=spl.join(' ');
+if(cmdName in socket.cmdsObj)cbObj2=socket.cmdsObj[cmdName];
+}
+}
+
+if(!cbObj2){
+if('' in socket.cmdsObj){
+spl=msgOrig.split(' ');
+cbObj2=socket.cmdsObj[''];
+}
+}
+
+if(cbObj2){
+if(!spl)spl=[];
+var res=cbObj2.cb(userObj,spl);
+if(res){
+var propsV=null;
+var typeV=null;
+if(typeof res=='object'){
+propsV=res;
+if(res.type){
+typeV=res.type;
+delete res.type;
+}
+if(typeV=='message')typeV=null;
+if(!typeV && res && res.msg)res=res.msg;
+}else if(typeof res=='number')res=''+res;
+
+if(typeof res!='undefined'){
+isEmpty=false;
+if(typeof res=='string'){
+sendData(socket,packid,{...propsV,cmd:'message',userid:userObj.id,msg:res});
+}
+}
+}
+}
+
+if(isEmpty)sendData(socket,packid,{cmd:'ok'});
+
+}
+}
+
+}
+
+}else if(cmdType==NetCmd.MSG_BINARY){
+var objData=null;
+var binData=null;
+try{
+var strSize=buffer.readUInt32LE(0);
+if(strSize>0){
+var pos2=4+strSize;
+var buffer2=buffer.subarray(4,pos2);
+objData=JSON.parse(buffer2.toString());
+if(pos2<buffer.length)binData=buffer.subarray(pos2);
+}
+}catch(e){
+}
+
+if(binData && objData && objData.user){
+if(socket.isLog)log('[packet binary '+packid+']','[data] ->',JSON.stringify(objData),binData,'['+binData.length+' bytes]');
+if('file' in socket.eventsObj)socket.eventsObj.file(objData,binData);
+}
+
+}
+
+}
+}while(checkFlag);
+}
+
+
+class Client{
+
+constructor(token){
+if(typeof token=='undefined' && process.argv.length>2)token=process.argv[2];
+this.client=null;
+this.botInfo=null;
+this.isLog=true;
+this.isConnected=false;
+this.token=token;
+this.connectHost='ag6.ru';
+this.connectPort=8000;
+this.reconnect=false;
+this.chunkInfo={messageSize:0, cmdType:0, packetID:0, buffer:Buffer.alloc(0)};
+this.callbacks={};
+this.eventsObj={};
+this.cmdsObj={};
+if(process.argv.length>3 && process.argv[3]=='local')this.connectHost='127.0.0.1';
+this.log=log;
+}
+
+event(type, cb){
+if(type && typeof cb=='function')this.eventsObj[type]=cb;
+}
+
+cmd(name, cb){
+if(typeof cb=='function'){
+this.cmdsObj[name]={cb:cb};
+}
+}
+
+api(method, props, cb){
+if(this.client){
+if(!props)props={};
+sendData(this,0,{...props,method:method,cmd:'api'},cb);
+}
+}
+
+sendGift(userid, itemid, cb){
+this.api('bot.sendGift',{userid:userid,itemid:itemid},cb);
+}
+
+sendMessage(userid, msg, props, cb){
+if(!props)props={};
+sendData(this,0,{...props,cmd:'message',userid:userid,msg:msg},cb);
+}
+
+sendFile(userid, info, data, cb){
+if(!info)info={};
+if(data && data instanceof Buffer){
+sendBinaryData(this,{...info,userid:userid},data,cb);
+}
+}
+
+sendFileName(userid, name, data, cb){
+if(data && data instanceof Buffer){
+sendBinaryData(this,{name:name,userid:userid},data,cb);
+}
+}
+
+run(cb){
+var th=this;
+th.reconnect=true;
+if(th.client)return;
+var sock=new Socket();
+sock.on('error', (e)=>{});
+sock.on('close', ()=>{
+var txt='ошибка подключения';
+if(th.isConnected)txt='соединение закрыто';
+log(txt);
+th.isConnected=false;
+if(th.reconnect){
+setTimeout(()=>{
+th.client=null;
+th.run(cb);
+}, 5000);
+}
+});
+sock.on('data', (data)=>{readBuffer(th,data,cb)});
+
+log('клиент ->','подключение к серверу...');
+
+sock.connect(th.connectPort,th.connectHost,()=>{
+th.isConnected=true;
+log('клиент ->','сервер, давай дружить?');
+sendPacket(th,0,NetCmd.FRIEND);
+});
+th.client=sock;
+}
+
+}
+
+module.exports={Bot:Client, log:log};
